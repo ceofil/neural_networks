872e65e8a4869776599466174644e81e6875ade2
ceofil <gceofil@gmail.com>
2020-10-15 02:35:25 +0300

    moved parser functionality to a separate file

--- assignments/asg_01/standard.py
+++ assignments/asg_01/standard.py
@@ -1,38 +1,4 @@
-import re
-
-
-def map_capture_to_coefficient(capture):
-    if capture == '':
-        return 1
-    if capture == '+':
-        return 1
-    if capture == '-':
-        return -1
-    if capture.startswith('+'):
-        return float(capture.lstrip('+'))
-    if capture.startswith('-'):
-        return -float(capture.lstrip('-'))
-    return float(capture)
-
-
-def parse_equation(path):
-    with open(path, 'r') as fd:
-        lines = [line.replace('\n', '').replace(' ', '')
-                 for line in fd.readlines()]
-
-        regex = r'([\+\-]?\d*\.?\d*)x([\+\-]\d*\.?\d*)y([\+\-]\d*\.?\d*)z=([\+\-]?\d*\.?\d*)'
-
-        raw_coefficients = []
-        for line in lines:
-            res = re.search(regex, line)
-            a, b, c, r = res.groups()
-            raw_coefficients.append([a, b, c, r])
-
-        output = []
-        for line in raw_coefficients:
-            output.append([map_capture_to_coefficient(c) for c in line])
-
-        return output
+from parse_utils import parse_equation
 
 
 def get_determinant2(matrix):
