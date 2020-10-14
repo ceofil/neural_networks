ed0c343b16eada98d6f4775b890ee4f0519bcd89
ceofil <gceofil@gmail.com>
2020-10-14 18:40:08 +0300

    modified regex to support floats

--- assignments/asg_01/standard.py
+++ assignments/asg_01/standard.py
@@ -9,10 +9,10 @@ def map_capture_to_coefficient(capture):
     if capture == '-':
         return -1
     if capture.startswith('+'):
-        return int(capture.lstrip('+'))
+        return float(capture.lstrip('+'))
     if capture.startswith('-'):
-        return -int(capture.lstrip('-'))
-    return int(capture)
+        return -float(capture.lstrip('-'))
+    return float(capture)
 
 
 def parse_equation(path):
@@ -20,7 +20,7 @@ def parse_equation(path):
         lines = [line.replace('\n', '').replace(' ', '')
                  for line in fd.readlines()]
 
-        regex = r'([\+\-]?\d*)x([\+\-]\d*)y([\+\-]\d*)z=([\+\-]?\d*)'
+        regex = r'([\+\-]?\d*\.?\d*)x([\+\-]\d*\.?\d*)y([\+\-]\d*\.?\d*)z=([\+\-]?\d*\.?\d*)'
 
         raw_coefficients = []
         for line in lines:
@@ -35,10 +35,11 @@ def parse_equation(path):
         return output
 
 
+# for testing purposes
 expected = [
-    [2, 3, 4, 0],
-    [1, 0, -2, -2],
-    [-1, -1, 1, 3]
+    [2.0, 3, 3.14, 0],
+    [1, 0, -2, -2.3],
+    [-1.1234, -0.524, 1, 3]
 ]
 
 result = parse_equation('input.txt')
