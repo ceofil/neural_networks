0a147e99d83eb2747f28c0ab21f48a0ca2b2f870
ceofil <gceofil@gmail.com>
2020-10-23 22:26:59 +0300

    merged all perceptron training in a single matrix of weights

--- assignments/asg_02/main.py
+++ assignments/asg_02/main.py
@@ -6,12 +6,13 @@ from game import Visualiser, COLOR, pygame
 
 pygame.init()
 size = 5
-game = Visualiser(28 * size * 2, 28 * size)
+game = Visualiser(28 * size * 10, 28 * size)
 
 
 def visualize(img, weights):
     game.visualize_image(img, (0, 0), size, COLOR.alpha_map((255, 255, 255)))
-    game.visualize_image(weights, (28 * size, 0), size, COLOR.weight_map(4))
+    for perceptron in range(10):
+        game.visualize_image(weights[:, perceptron], (28 * size * perceptron, 0), size, COLOR.weight_map(4))
     game.update()
 
 
@@ -32,17 +33,21 @@ for img, result in zip(imgs, results):
         data[result] = []
     data[result].append(img)
 
-weights = np.zeros(28 * 28)
-bias = 0
+weights = np.zeros([28 * 28, 10])
+bias = np.zeros(10)
 
-perceptron_idx = 3
 for img, result, idx in zip(imgs, results, range(len(imgs))):
     x = img
     z = np.dot(x, weights) + bias
-    output = 1 if z > 0 else 0
-    t = 1 if result == perceptron_idx else 0
+
+    output = np.array([1 if i > 0 else 0 for i in z])  # z is a 1d array of length, loops wont affect performance
+
+    t = np.zeros(10)
+    t[result] = 1
 
     epsilon = 0.5
+
+    x = np.broadcast_to(np.reshape(x, (len(x), 1)), (28 * 28, len(output)))
     weights = weights + x * (t - output) * epsilon
     bias = bias + (t - output) * epsilon
 
@@ -50,6 +55,4 @@ for img, result, idx in zip(imgs, results, range(len(imgs))):
         visualize(img, weights)
         print(idx)
 
-
 visualize(img, weights)
-print(idx)
