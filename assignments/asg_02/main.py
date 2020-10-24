948164a4d4f7854f09618ef8e71cdf2e614cd797
ceofil <gceofil@gmail.com>
2020-10-24 18:35:20 +0300

    separated rendering logic

--- assignments/asg_02/main.py
+++ assignments/asg_02/main.py
@@ -4,27 +4,38 @@ import numpy as np
 import os
 from game import Visualiser, COLOR, pygame
 
-pygame.init()
+
+class LayerRendering:
+    def __init__(self):
+        pygame.init()
+        self.size_on_screen = 4
+        self.game = Visualiser(28 * self.size_on_screen * 10, 28 * self.size_on_screen)
 
 
 class Layer:
-    def __init__(self):
+    def __init__(self, view_progress):
         self.weights = np.zeros([28 * 28, 10])
         self.bias = np.zeros(10)
         self.delta = 0.6
-        self.size_on_screen = 4
-        self.game = Visualiser(28 * self.size_on_screen * 10, 28 * self.size_on_screen)
-        self.view_progress = True
+        self._render = None
+        self.view_progress = view_progress
         path = os.path.join('data', 'mnist.pkl.gz')
         with gzip.open(path, 'rb') as f:
             self.train_set, self.valid_set, self.test_set = pickle.load(f, encoding='latin')
 
+    @property
+    def render(self):
+        if not self._render:
+            self._render = LayerRendering()
+        return self._render
+
     def visualize(self):
-        # self.game.visualize_image(img, (0, 0), self.size_on_screen, COLOR.alpha_map((255, 255, 255)))
+        # self.render.game.visualize_image(img, (0, 0), self.size_on_screen, COLOR.alpha_map((255, 255, 255)))
         for perceptron in range(10):
-            self.game.visualize_image(self.weights[:, perceptron], (28 * self.size_on_screen * perceptron, 0),
-                                      self.size_on_screen, COLOR.weight_map(4))
-        self.game.update()
+            self.render.game.visualize_image(self.weights[:, perceptron],
+                                             (28 * self.render.size_on_screen * perceptron, 0),
+                                             self.render.size_on_screen, COLOR.weight_map(4))
+        self.render.game.update()
 
     def train(self):
         images, results = self.train_set
@@ -33,9 +44,9 @@ class Layer:
         for img, result, idx in zip(images, results, range(iterations)):
             x = img
             z = np.dot(x, self.weights) + self.bias
-            
+
             # z is a 1d array of length, loops wont affect performance
-            output = np.array( [1 if i > 0 else 0 for i in z])
+            output = np.array([1 if i > 0 else 0 for i in z])
 
             t = np.zeros(10)
             t[result] = 1
@@ -44,13 +55,14 @@ class Layer:
             self.weights = self.weights + x * (t - output) * self.delta
             self.bias = self.bias + (t - output) * self.delta
 
-            if idx % 50 == 0 or idx + 1 == iterations:
+            if idx % 10000 == 0 or idx + 1 == iterations:
                 print(f'{idx}/{iterations}')
                 if self.view_progress:
                     self.visualize()
 
 
 if __name__ == '__main__':
-    layer = Layer()
+    layer = Layer(view_progress=False)
     layer.train()
     layer.visualize()
+    layer.render.game.run()  # this allows pygame event loop to run
