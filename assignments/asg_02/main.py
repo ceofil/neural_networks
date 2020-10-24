fb408fa8011bb1b99742d5d47feff89eccea1246
ceofil <gceofil@gmail.com>
2020-10-24 17:18:58 +0300

    reorganized the code

--- assignments/asg_02/main.py
+++ assignments/asg_02/main.py
@@ -5,54 +5,52 @@ import os
 from game import Visualiser, COLOR, pygame
 
 pygame.init()
-size = 5
-game = Visualiser(28 * size * 10, 28 * size)
 
 
-def visualize(img, weights):
-    game.visualize_image(img, (0, 0), size, COLOR.alpha_map((255, 255, 255)))
-    for perceptron in range(10):
-        game.visualize_image(weights[:, perceptron], (28 * size * perceptron, 0), size, COLOR.weight_map(4))
-    game.update()
+class Layer:
+    def __init__(self):
+        self.weights = np.zeros([28 * 28, 10])
+        self.bias = np.zeros(10)
+        self.delta = 0.6
+        self.size_on_screen = 4
+        self.game = Visualiser(28 * self.size_on_screen * 10, 28 * self.size_on_screen)
+        self.view_progress = True
+        path = os.path.join('data', 'mnist.pkl.gz')
+        with gzip.open(path, 'rb') as f:
+            self.train_set, self.valid_set, self.test_set = pickle.load(f, encoding='latin')
+
+    def visualize(self):
+        # self.game.visualize_image(img, (0, 0), self.size_on_screen, COLOR.alpha_map((255, 255, 255)))
+        for perceptron in range(10):
+            self.game.visualize_image(self.weights[:, perceptron], (28 * self.size_on_screen * perceptron, 0),
+                                      self.size_on_screen, COLOR.weight_map(4))
+        self.game.update()
+
+    def train(self):
+        images, results = self.train_set
+        iterations = len(images)
+
+        for img, result, idx in zip(images, results, range(iterations)):
+            x = img
+            z = np.dot(x, self.weights) + self.bias
+            
+            # z is a 1d array of length, loops wont affect performance
+            output = np.array( [1 if i > 0 else 0 for i in z])
+
+            t = np.zeros(10)
+            t[result] = 1
+
+            x = np.broadcast_to(np.reshape(x, (len(x), 1)), (28 * 28, len(output)))
+            self.weights = self.weights + x * (t - output) * self.delta
+            self.bias = self.bias + (t - output) * self.delta
+
+            if idx % 50 == 0 or idx + 1 == iterations:
+                print(f'{idx}/{iterations}')
+                if self.view_progress:
+                    self.visualize()
 
 
-def get_stuff():
-    path = os.path.join('data', 'mnist.pkl.gz')
-    with gzip.open(path, 'rb') as f:
-        train_set, valid_set, test_set = pickle.load(f, encoding='latin')
-
-    return train_set, valid_set, test_set
-
-
-train_set, valid_set, test_set = get_stuff()
-
-imgs, results = train_set
-data = dict()
-for img, result in zip(imgs, results):
-    if result not in data:
-        data[result] = []
-    data[result].append(img)
-
-weights = np.zeros([28 * 28, 10])
-bias = np.zeros(10)
-
-for img, result, idx in zip(imgs, results, range(len(imgs))):
-    x = img
-    z = np.dot(x, weights) + bias
-
-    output = np.array([1 if i > 0 else 0 for i in z])  # z is a 1d array of length, loops wont affect performance
-
-    t = np.zeros(10)
-    t[result] = 1
-
-    epsilon = 0.5
-
-    x = np.broadcast_to(np.reshape(x, (len(x), 1)), (28 * 28, len(output)))
-    weights = weights + x * (t - output) * epsilon
-    bias = bias + (t - output) * epsilon
-
-    if idx % 50 == 0:
-        visualize(img, weights)
-        print(idx)
-
-visualize(img, weights)
+if __name__ == '__main__':
+    layer = Layer()
+    layer.train()
+    layer.visualize()
